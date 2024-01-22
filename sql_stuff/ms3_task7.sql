659ad0ef722b1a67ddef05920598f0f364b5734a
samierouf <142994082+samierouf@users.noreply.github.com>
2024-01-22 11:29:11 +0000

    milestone 3

---
+++ sql_stuff/ms3_task7.sql
@@ -0,0 +1,7 @@
+ALTER TABLE dim_card_details 
+    -- SELECT MAX(LENGTH(card_number::TEXT)) FROM dim_card_details;
+    ALTER COLUMN card_number TYPE VARCHAR(19),
+    -- SELECT MAX(LENGTH(expiry_date::TEXT)) FROM dim_card_details;
+    ALTER COLUMN expiry_date TYPE VARCHAR(25), -- i think it should be TYPE DATE
+    ALTER COLUMN date_payment_confirmed TYPE DATE;
+
