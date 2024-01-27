4970e6ab015302cd66ef10f27a01d063e79c9452
samierouf <142994082+samierouf@users.noreply.github.com>
2024-01-27 17:17:35 +0000

    Delete sql_stuff directory

--- sql_stuff/ms3_task2.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE dim_users
-    ALTER COLUMN first_name TYPE VARCHAR(255),
-    ALTER COLUMN last_name TYPE VARCHAR(255),
-    ALTER COLUMN date_of_birth TYPE DATE USING date_of_birth::DATE,
-    ALTER COLUMN country_code TYPE VARCHAR(3),
-    ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID,
-    ALTER COLUMN join_date TYPE DATE;
